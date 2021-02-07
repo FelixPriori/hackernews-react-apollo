29148df08273766f177d158077a1061df7aec633
felix <felix@webisoft.com>
2021-02-07 12:24:48 -0500

    implement routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,21 @@
 import '../styles/App.css'
+import CreateLink from './CreateLink'
 import LinkList from './LinkList'
+import Header from './Header'
+import {Switch, Route} from 'react-router-dom'
 
 function App() {
-  return <LinkList />
+  return (
+    <div className="center w85">
+      <Header />
+      <div className="ph3 pv1 background-gray">
+        <Switch>
+          <Route exact path="/" component={LinkList} />
+          <Route exact path="/create" component={CreateLink} />
+        </Switch>
+      </div>
+    </div>
+  )
 }
 
 export default App
