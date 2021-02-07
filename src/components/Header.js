d0f2e992f749fa3705b530557e1470b98346fce5
felix <felix@webisoft.com>
2021-02-07 12:54:56 -0500

    fetch token from localstorage to determine ui

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,17 @@
 import React from 'react'
 import {useHistory} from 'react-router'
-import {Link, withRouter} from 'react-router-dom'
+import {Link} from 'react-router-dom'
+import {AUTH_TOKEN} from '../constants'
 
 function Header() {
   const history = useHistory()
+
+  /* 
+    WARNING
+    Storing JWTs in localStorage is not a safe approach to implement authentication on the frontend. 
+    Because this tutorial is focused on GraphQL, we want to keep things simple and therefore are using it here.
+  */
+  const authToken = localStorage.getItem(AUTH_TOKEN)
   return (
     <div className="flex pa1 justify-between nowrap orange">
       <div className="flex flex-fixed black">
@@ -15,6 +23,23 @@ function Header() {
         <Link to="/create" className="ml1 no-underline black">
           submit
         </Link>
+        <div className="flex flex-fixed">
+          {authToken ? (
+            <div
+              className="ml1 pointer black"
+              onClick={() => {
+                localStorage.removeItem(AUTH_TOKEN)
+                history.push(`/`)
+              }}
+            >
+              logout
+            </div>
+          ) : (
+            <Link to="login" className="ml1 no-underline black">
+              login
+            </Link>
+          )}
+        </div>
       </div>
     </div>
   )
