3820c226fa26f4f292097611466d77d5e8eb7637
felix <felix@webisoft.com>
2021-02-08 17:01:41 -0500

    setup websockets

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {BrowserRouter} from 'react-router-dom'
-import {ApolloProvider, ApolloClient, createHttpLink, InMemoryCache} from '@apollo/client'
+import {ApolloProvider, ApolloClient, createHttpLink, InMemoryCache, split} from '@apollo/client'
+import {getMainDefinition} from '@apollo/client/utilities'
+import {WebSocketLink} from '@apollo/client/link/ws'
 import {setContext} from '@apollo/client/link/context'
 import './styles/index.css'
 import App from './components/App'
@@ -27,7 +29,26 @@ const authLink = setContext((_, {headers}) => {
   }
 })
 
-const client = new ApolloClient({link: authLink.concat(httpLink), cache: new InMemoryCache()})
+const wsLink = new WebSocketLink({
+  uri: `ws://localhost:4000/graphql`,
+  options: {
+    reconnect: true,
+    connectionParams: {
+      authToken: localStorage.getItem(AUTH_TOKEN),
+    },
+  },
+})
+
+const link = split(
+  ({query}) => {
+    const {kind, operation} = getMainDefinition(query)
+    return kind === 'OperationDefinition' && operation === 'subscription'
+  },
+  wsLink,
+  authLink.concat(httpLink),
+)
+
+const client = new ApolloClient({link, cache: new InMemoryCache()})
 
 ReactDOM.render(
   <BrowserRouter>
