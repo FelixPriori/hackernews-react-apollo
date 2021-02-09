f63e20ed702228ef821281b404e578aa5876575a
felix <felix@webisoft.com>
2021-02-09 10:42:19 -0500

    seperate queries, subscriptions and mutation in own files

---
+++ src/queries.js
@@ -0,0 +1,50 @@
+import gql from 'graphql-tag'
+
+export const FEED_SEARCH_QUERY = gql`
+  query FeedSearchQuery($filter: String!) {
+    feed(filter: $filter) {
+      id
+      links {
+        id
+        url
+        description
+        createdAt
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+    }
+  }
+`
+
+export const FEED_QUERY = gql`
+  query FeedQuery($take: Int, $skip: Int, $orderBy: LinkOrderByInput) {
+    feed(take: $take, skip: $skip, orderBy: $orderBy) {
+      id
+      links {
+        id
+        createdAt
+        url
+        description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      count
+    }
+  }
+`
