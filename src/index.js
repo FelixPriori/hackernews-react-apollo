946a104bee0adb61fa7082e8fd7decb62b129743
felix <felix@webisoft.com>
2021-02-14 12:47:36 -0500

    add some error handling

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ import {ApolloProvider, ApolloClient, createHttpLink, InMemoryCache, split} from
 import {getMainDefinition} from '@apollo/client/utilities'
 import {WebSocketLink} from '@apollo/client/link/ws'
 import {setContext} from '@apollo/client/link/context'
+import {onError} from 'apollo-link-error'
+import {ToastContainer, toast} from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import './styles/index.css'
 import App from './components/App'
 import reportWebVitals from './reportWebVitals'
@@ -12,6 +15,15 @@ import {AUTH_TOKEN} from './constants'
 
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000',
+})
+
+const errorLink = onError(({graphQLErrors, networkError}) => {
+  if (graphQLErrors)
+    graphQLErrors.forEach(({message, locations, path}) => {
+      console.error(`[GraphQL error]: Message: ${message}, Location: ${JSON.stringify(locations)}, Path: ${path}`)
+      toast.error(message)
+    })
+  if (networkError) console.error(`[Network error]: ${networkError}`)
 })
 
 /* 
@@ -45,7 +57,7 @@ const link = split(
     return kind === 'OperationDefinition' && operation === 'subscription'
   },
   wsLink,
-  authLink.concat(httpLink),
+  errorLink.concat(authLink.concat(httpLink)),
 )
 
 const client = new ApolloClient({link, cache: new InMemoryCache()})
@@ -54,6 +66,7 @@ ReactDOM.render(
   <BrowserRouter>
     <ApolloProvider client={client}>
       <App />
+      <ToastContainer />
     </ApolloProvider>
   </BrowserRouter>,
   document.getElementById('root'),
